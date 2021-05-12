dea17a6930635da5157b3fe1f44600e353b22d38
Aleksandrs Barkancevs <barkancev.alexander@gmail.com>
2021-05-12 12:48:09 +0300

    Добавлена кнопка скрола до верха и стили

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ import './css/footer.css';
 import './css/container.css';
 import './css/modal.css';
 import './css/form.css';
+import './css/back-to-top-btn.css';
 import './css/pagination.css';
 
 // import JS-files
 import './js/buildHtml.js';
 import './js/modal-open.js';
 import './js/country-down.js';
+import './js/back-to-top-btn.js';
