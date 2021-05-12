e885bfe37df76898919c9be180c1b53d36e68e71
KatePodolsky <Doogmatix91@gmail.com>
2021-05-12 13:50:58 +0300

    Fix misprint

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -6,15 +6,14 @@ export default class ApiService {
     }
     
     fetchEvents() {
-    const BASE_URL = 'https://newsuperserver.herokuapp.com/https://newsuperserver.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json';
+    const BASE_URL = 'https://newsuperserver.herokuapp.com/https://app.ticketmaster.com/discovery/v2/events.json';
     const API_KEY = 'HxNpbFKA9sq5XW5ZGrD232EXapf1LXRh'
         
     return fetch(`${BASE_URL}?keyword=${this.searchQuery}&source=universe&size=${this.size}&page=${this.page}&apikey=${API_KEY}`)
         .then(response => response.json())
         .then(({_embedded}) => {
             this.page += 1;
-            const card = _embedded.events
-             return card
+             return _embedded.events
          })
         .catch(error => console.log(error))
 
