d003773db8578e631b09123a0e277f8a056f3c8d
ivan_filipchyk <ivanfiliptschik00@gmail.com>
2021-05-16 07:29:32 +0300

    back-to-top-btn
    file out

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,8 @@
 import './page-segments/build-html.js';
 import './page-segments/modal-open.js';
 import './page-segments/country-down.js';
+import './page-segments/back-to-top-btn.js';
+
 
 // import JS logic
 import './logic/search-events.js';
-import './logic/back-to-top-btn.js';
