01a926166b9c7419574a326b545f2e2179fad6fe
ivan_filipchyk <ivanfiliptschik00@gmail.com>
2021-05-16 08:24:44 +0300

    modal-refactoring

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,10 @@
 // import JS for page render
 import './page-segments/build-html.js';
-import './page-segments/modal-open.js';
 import './page-segments/country-down.js';
 import './page-segments/back-to-top-btn.js';
 
 
 // import JS logic
 import './logic/search-events.js';
+import './scripts/modal.js'
+import './logic/modal-open.js';
---
+++ src/js/scripts/modal.js
@@ -0,0 +1,58 @@
+import refs from '../refs.js';
+
+const openModalAnimation = () => {
+  refs.modalCard.classList.replace('is-closed', 'is-open')
+};
+
+const closeModalAnimation = () => {
+  refs.modalCard.classList.replace('is-open', 'is-closed')
+};
+
+const bodyStopScroll = () => {
+  document.body.style.overflow = 'hidden';
+};
+
+const bodyReturnScroll = () => {
+  document.body.style.overflow = 'visible';
+};
+
+const openModal = () => {
+  refs.modalContainer.classList.remove('visually-hidden');
+  openModalAnimation();
+  bodyStopScroll();
+};
+
+const closeModal = () => {
+  setTimeout(() => {
+    refs.modalContainer.classList.add('visually-hidden');
+  }, 250);
+  
+  closeModalAnimation();
+  bodyReturnScroll();
+};
+
+const onCardClick = (e) => {
+    openModal();
+};
+
+const onCloseBtnClick = () => {
+    closeModal();
+};
+
+const onOverlayClick = (e) => {
+  if (e.target === refs.backdrop) {
+    modal.onCloseBtnClick();
+  }
+};
+
+export {
+  openModal,
+  closeModal,
+  openModalAnimation,
+  closeModalAnimation,
+  bodyStopScroll,
+  bodyReturnScroll,
+  onCardClick,
+  onCloseBtnClick,
+  onOverlayClick,
+};
