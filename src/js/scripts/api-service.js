51067392a26e56ac56aa1e806e780523ff50055e
KatePodolsky <Doogmatix91@gmail.com>
2021-05-17 18:48:42 +0300

    Add pnotify ("@pnotify/core": "^5.2.0")

--- src/js/scripts/api-service.js
+++ src/js/scripts/api-service.js
@@ -1,4 +1,5 @@
-import { searchEvents }  from './api-fetch.js';
+import {pnotifyMessage} from './pnotify.js';
+import { searchEvents } from './api-fetch.js';
 import refs from '../refs';
 import Url from '../logic/url-factoring';
 import toggleType from '../scripts/toggle-event-type';
@@ -118,8 +119,8 @@ export default class ApiService {
         }
         // Если ничего не выбрано
         if (!this.countryCode && this.searchQuery === "") {
-            return console.log('Вы ничего не ввели');
-            // pnotifyMessage(error, 'Please enter query!')
+            return pnotifyMessage('empty input')
+           
         }
         
         console.log(result);
