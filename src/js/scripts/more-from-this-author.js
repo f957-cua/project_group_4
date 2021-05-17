7033bc3f871185b350e991d5d7dedbaefe562f21
iborovikov <ivans.borovikovs@live.com>
2021-05-17 10:11:00 +0300

    Добавил переменную, имя автора

--- src/js/scripts/more-from-this-author.js
+++ src/js/scripts/more-from-this-author.js
@@ -3,7 +3,6 @@ import eventCardTpl from '../../template/event-card.hbs';
 import * as modalOptions from './modal.js';
 import EventsApiService from './api-service.js';
 import { eventsMarkUp } from './mark-up.js';
-import { onSearch } from './search-functions.js';
 
 const fetchApi = new EventsApiService();
 
@@ -12,8 +11,8 @@ async function onMoreFromAuthorBtnClick(e) {
     
     const authorName = e.target.previousElementSibling.children[0].alt;
     console.log(authorName)
-    // onSearch(authorName)
-    const events = await fetchApi.fetchEvents(authorName);
+    fetchApi.query = authorName
+    const events = await fetchApi.fetchEvents();
     console.log(events)
     eventsMarkUp(events);
     
