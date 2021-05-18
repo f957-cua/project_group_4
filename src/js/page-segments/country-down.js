31a21eb02099cf27428aef8a6e192b64cc0a9c70
Oleg Shevchenko <ama.freelnc@gmail.com>
2021-05-19 00:53:35 +0300

    Пофиксил фокусы

--- src/js/page-segments/country-down.js
+++ src/js/page-segments/country-down.js
@@ -1,9 +1,34 @@
 
 import refs from '../refs.js';
+let activeListItem = 1;
 
 refs.select.addEventListener('click', (e) => customSelect(e));
+refs.select.addEventListener('focus', () => refs.select.classList.add('is-active'));
+refs.select.addEventListener('focusout', () => refs.select.classList.remove('is-active'));
+// refs.select.addEventListener('keydown', onArrowBtnPress(e));
 
+// const onArrowBtnPress = (event) => {
+//     if (!refs.select.classList.contains('is-active')) {
+//         return;
+//     };
 
+//     if (event.key === "ArrowDown") {
+//         if (activeListItem === 11) {
+//             activeListItem = 1;
+//         } else {
+//             activeListItem += 1;
+//         };
+//     } else if (event.key === "ArrowUp") {
+//         if (activeListItem === 1) {
+//             activeListItem = 11;
+//         } else {
+//             activeListItem -= 1;
+//         };
+//     };
+
+//     refs.select.childNodes[activeListItem+1].focus()
+// }
+    
 const classListToggle = ( selector ) => {
    selector.classList.toggle('is-active');
 }
