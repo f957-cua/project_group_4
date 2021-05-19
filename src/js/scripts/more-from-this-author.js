aea14a0163fa28ea403e3a79d12e6e73698e3269
deniskodak <denis.kodak@gmail.com>
2021-05-20 01:23:33 +0300

    fixed modal

--- src/js/scripts/more-from-this-author.js
+++ src/js/scripts/more-from-this-author.js
@@ -3,18 +3,34 @@ import {closeModal} from './modal.js';
 import EventsApiService from './api-service.js';
 import { eventsMarkUp } from './mark-up.js';
 import { loader } from './loader.js';
+import { hideBtn } from './hide-paginator-btn';
+import { filterArray } from './search-functions';
+import { scrollToTop } from '../page-segments/back-to-top-btn.js';
 
 const fetchApi = new EventsApiService();
 
 const onMoreFromAuthorBtnClick = async (e) => {
   
     if (e.target.classList.contains('modal__more-from-btn')) {
+        try {
         const authorName = e.target.dataset.author;
         fetchApi.searchQuery = authorName
         const events = await fetchApi.mainFetch(authorName);
+        localStorage.setItem('array', JSON.stringify(events));
+        const type = events[0].type;
         closeModal(e);
         loader.show();
-        eventsMarkUp(events);
+        eventsMarkUp(filterArray(events, type));
+        hideBtn();
+            // if (refs.filterContainer.classList.contains('visually-hidden')) {
+            refs.filterContainer.classList.remove('visually-hidden');
+        // }
+        }
+        catch {
+        closeModal(e);
+        loader.hide();
+        scrollToTop();
+        }
     }
 }
 
